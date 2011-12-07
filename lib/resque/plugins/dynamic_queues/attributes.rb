e85f6a033642e9fa323826b2bc359d106ba876b4
Matt Conway <wr0ngway@yahoo.com>
2011-12-07 10:21:20 -0500

    make settnig of fallback more explicit in web ui

--- lib/resque/plugins/dynamic_queues/attributes.rb
+++ lib/resque/plugins/dynamic_queues/attributes.rb
@@ -1,13 +1,17 @@
 module Resque
   module Plugins
     module DynamicQueues
+
+      KEY_PREFIX = "dynamic_queue"
+      FALLBACK_KEY = "default"
+
       module Attributes
 
         def get_dynamic_queue(key)
-          queue_names = redis.lrange("dynamic_queue:#{key}", 0, -1)
+          queue_names = redis.lrange("#{KEY_PREFIX}:#{key}", 0, -1)
 
           if queue_names.nil? || queue_names.size == 0
-            queue_names = redis.lrange("dynamic_queue:default", 0, -1)
+            queue_names = redis.lrange("#{KEY_PREFIX}:#{FALLBACK_KEY}", 0, -1)
           end
           
           if queue_names.nil? || queue_names.size == 0
@@ -18,15 +22,20 @@ module Resque
         end
 
         def set_dynamic_queue(key, values)
-          k = "dynamic_queue:#{key}"
-          redis.del(k)
-          Array(values).each do |v|
-             redis.rpush(k, v)
+          k = "#{KEY_PREFIX}:#{key}"
+          redis.multi do
+            redis.del(k)
+            Array(values).each do |v|
+               redis.rpush(k, v)
+            end
           end
         end
 
         def get_dynamic_queues
-          (redis.keys("dynamic_queue:*") || []).collect{|q| q.gsub('dynamic_queue:', '')}
+          dqueues = redis.keys("#{KEY_PREFIX}:*") || []
+          dqueues = dqueues.collect{|q| q.gsub('dynamic_queue:', '')}
+          dqueues << FALLBACK_KEY unless dqueues.include?(FALLBACK_KEY)
+          return dqueues
         end
 
       end
