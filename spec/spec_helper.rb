e8c0c163f09a8318c4dcd2e19bf07a2a5882288b
Matt Conway <wr0ngway@yahoo.com>
2012-02-01 07:50:09 -0500

    enable travis ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,22 @@
 require 'rspec'
 require 'resque-dynamic-queues'
 
-spec_dir = File.dirname(File.expand_path(__FILE__))
-REDIS_CMD = "redis-server #{spec_dir}/redis-test.conf"
+# No need to start redis when running in Travis
+unless ENV['CI']
 
-puts "Starting redis for testing at localhost:9736..."
-puts `cd #{spec_dir}; #{REDIS_CMD}`
-Resque.redis = 'localhost:9736'
+  spec_dir = File.dirname(File.expand_path(__FILE__))
+  REDIS_CMD = "redis-server #{spec_dir}/redis-test.conf"
+  
+  puts "Starting redis for testing at localhost:9736..."
+  puts `cd #{spec_dir}; #{REDIS_CMD}`
+  Resque.redis = 'localhost:9736'
+  
+  # Schedule the redis server for shutdown when tests are all finished.
+  at_exit do
+    pid = File.read("#{spec_dir}/redis.pid").to_i rescue nil
+    system ("kill #{pid}") if pid != 0
+  end
 
-# Schedule the redis server for shutdown when tests are all finished.
-at_exit do
-  pid = File.read("#{spec_dir}/redis.pid").to_i rescue nil
-  system ("kill #{pid}") if pid != 0
 end
 
 class SomeJob
