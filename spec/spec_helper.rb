21ec8e06749d9b9bc40938064202e62640ffb1e6
Matt Conway <wr0ngway@yahoo.com>
2012-02-02 06:30:06 -0500

    only auto start/stop redis if not on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,19 +4,26 @@ require 'resque-dynamic-queues'
 # No need to start redis when running in Travis
 unless ENV['CI']
 
-  spec_dir = File.dirname(File.expand_path(__FILE__))
-  REDIS_CMD = "redis-server #{spec_dir}/redis-test.conf"
-  
-  puts "Starting redis for testing at localhost:9736..."
-  puts `cd #{spec_dir}; #{REDIS_CMD}`
-  Resque.redis = 'localhost:9736'
-  
-  # Schedule the redis server for shutdown when tests are all finished.
-  at_exit do
-    pid = File.read("#{spec_dir}/redis.pid").to_i rescue nil
-    system ("kill #{pid}") if pid != 0
+  begin
+    Resque.queues
+  rescue Errno::ECONNREFUSED
+    spec_dir = File.dirname(File.expand_path(__FILE__))
+    REDIS_CMD = "redis-server #{spec_dir}/redis-test.conf"
+    
+    puts "Starting redis for testing at localhost..."
+    puts `cd #{spec_dir}; #{REDIS_CMD}`
+    
+    # Schedule the redis server for shutdown when tests are all finished.
+    at_exit do
+      puts 'Stopping redis'
+      pid = File.read("#{spec_dir}/redis.pid").to_i rescue nil
+      system ("kill -9 #{pid}") if pid.to_i != 0
+      File.delete("#{spec_dir}/redis.pid") rescue nil
+      File.delete("#{spec_dir}/redis-server.log") rescue nil
+      File.delete("#{spec_dir}/dump.rdb") rescue nil
+    end
   end
-
+  
 end
 
 class SomeJob
